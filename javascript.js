13682319dd809b76f3511c6586eb2ed92a6e8aa5
Latane <1thisisfornothing1@gmail.com>
2015-10-20 17:34:02 -0500

    Commit 2

---
+++ javascript.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+	
+	var wndheight = window.innerHeight;
+	var wndwidth = window.innerWidth;
+
+	var div1 = document.createElement('div');
+	div1.style.height = '50px';
+	div1.style.width = '50px';
+	div1.style.background = 'red';
+	
+
+	for (var i = 16; i > 0; i--) {
+		var griddiv[i] = document.createElement('div');
+		griddiv[i].style.height = wndheight/16;
+		griddiv[i].style.width = wndwidth/16;
+		griddiv[i].style.display = 'block';
+		griddiv[i].style.background = 'red';
+	};
+
+
+
+});
